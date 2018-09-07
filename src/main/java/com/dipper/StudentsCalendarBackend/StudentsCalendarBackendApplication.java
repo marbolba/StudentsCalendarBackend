56046949638bafefae326a845b9d76d4b5ddf404
Marcin Bołba <marcin@MarcinPc.dipper.com>
2018-09-07 15:49:26 +0200

    initial commit

---
+++ src/main/java/com/dipper/StudentsCalendarBackend/StudentsCalendarBackendApplication.java
@@ -0,0 +1,12 @@
+package com.dipper.StudentsCalendarBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsCalendarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsCalendarBackendApplication.class, args);
+	}
+}
